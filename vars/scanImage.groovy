8660a713be7afcfda3975dec030519a16cf00dbd
salma-netizen <engsalmaelsayed@gmail.com>
2025-09-09 09:52:11 +0300

    second

--- vars/scanImage.groovy
+++ vars/scanImage.groovy
@@ -1,6 +1,7 @@
 def call(String IMAGE_NAME) {
     sh """
-        /usr/local/bin/trivy --version
-       /usr/local/bin/trivy image ${IMAGE_NAME}
+        docker run --rm \\
+          -v /var/run/docker.sock:/var/run/docker.sock \\
+          aquasec/trivy image ${IMAGE_NAME}
     """
 }
