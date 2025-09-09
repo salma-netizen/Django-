6be4702f77c07eef609875e02679cef8dfe6ba7d
salma-netizen <engsalmaelsayed@gmail.com>
2025-09-09 04:48:17 +0300

    second

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,5 +1,5 @@
 def call() {
     echo "Building Docker image..."
-    sh "docker build -t $IMAGE_NAME docker/blog/Dockerfile docker/blog"
+    sh "docker build -t $IMAGE_NAME -f docker/blog/Dockerfile docker/blog"
     echo "Docker image built successfully!"
 }
