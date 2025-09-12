27af1e3f7bdffb795083f84baa0df2b4753115f0
salma-netizen <engsalmaelsayed@gmail.com>
2025-09-12 05:50:02 +0300

    second

--- vars/updateManifests.groovy
+++ vars/updateManifests.groovy
@@ -1,12 +1,12 @@
 def call() {
     echo "Updating Kubernetes manifests..."
 
-    def deploymentFile = readFile('k8s\deployment-djanjo.yml')
+    def deploymentFile = readFile('k8s/deployment-djanjo.yml')
 
     def newImageTag = "my-app:${env.BUILD_NUMBER}"
     deploymentFile = deploymentFile.replaceAll("my-app:.*", "my-app:${newImageTag}")
 
-    writeFile(file: 'k8s\deployment-djanjo.yml', text: deploymentFile)
+    writeFile(file: 'k8s/deployment-djanjo.yml', text: deploymentFile)
 
     echo "Kubernetes deployment updated with new image tag: ${newImageTag}"
 
