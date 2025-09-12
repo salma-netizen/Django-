646549036097e04172f75d6abcdabbeb1b0514e8
salma-netizen <engsalmaelsayed@gmail.com>
2025-09-12 04:10:59 +0300

    second

--- vars/scanImage.groovy
+++ vars/scanImage.groovy
@@ -1,4 +1,8 @@
-def call(String IMAGE_NAME) {
+def scanImage(String IMAGE_NAME) {
+    if (!IMAGE_NAME || IMAGE_NAME == "null") {
+        error("IMAGE_NAME is not set correctly. Please provide a valid image name.")
+    }
+
     sh """
         docker run --rm \\
           -v /var/run/docker.sock:/var/run/docker.sock \\
