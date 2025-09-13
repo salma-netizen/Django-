44e9cdb723f23b2b776e39ef54c6c926ad37955e
salma-netizen <engsalmaelsayed@gmail.com>
2025-09-13 10:09:08 +0300

    second

---
+++ terraform-infra/backendinfra.tf
@@ -0,0 +1,37 @@
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "salma-terraform-backend"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_encryption" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tf_state_versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "TerraformLockTable"
+  }
+}
